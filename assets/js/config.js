5b823acc1cdd8e3a70f46f78d28ee09709696a8a
Seobeen Jang <saudko-1@naver.com>
2026-03-18 11:27:51 +0900

    Update upload configuration for new repo and URLs

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.UPLOAD_CONFIG = {
-  repoName: "filter-images-template",
-  galleryBaseUrl: "https://filter-log.github.io/filter-images-template",
-  workerApiUrl: "",
+  repoName: "filter-images-1",
+  galleryBaseUrl: "https://filter-log.github.io/filter-images-1",
+  workerApiUrl: "https://filter-image-upload-worker.filter-log.workers.dev/",
   maxFiles: 100,
 };
